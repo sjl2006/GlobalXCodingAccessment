8c38aabc7c2b21ae7beda9744b78c08a9d738444
sjl2006 <shengjiale@gmail.com>
2018-02-11 21:43:48 +1100

    Source code, solution, project and test data for NameSorter and its unit test

---
+++ NameSorter/MSUnitTest.NameSorter/NameSorterTest.cs
@@ -0,0 +1,321 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NameSorterApp;
+
+namespace NameSorterApp.MSUnitTest
+{
+    [TestClass]
+    public class SortNames_UnitTest
+    {
+        private readonly SortNames InstSortNames;
+
+        public SortNames_UnitTest() => InstSortNames = new SortNames();
+
+        /// <summary>
+        /// check if the data and order is identical
+        /// </summary>
+        /// <param name="ListA">List A</param>
+        /// <param name="ListB">List B</param>
+        /// <returns>true, false</returns>
+        private bool ListCompare(ref List<NameInfo> ListA, ref List<NameInfo> ListB)
+        {
+            bool Result = true;
+
+            if (ListA.Count == ListB.Count)
+            {
+                for (int i = 0; i < ListA.Count; i++)
+                {
+                    if ((ListA[i].LastName != ListB[i].LastName)
+                    || (ListA[i].GivenName != ListB[i].GivenName))
+                    {
+                        Result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Result = false;
+            }
+            return Result;
+        }
+
+        [TestMethod]
+        /// <summary> Test when input file path string is null</summary>
+        public void TestGetNameListFromFile_NullInputPath()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            string InputPath = null;
+            RetCode retCode;
+
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when input list is null</summary>
+        public void TestGetNameListFromFile_NullList()
+        {
+            List<NameInfo> NameList = null;
+            string InputPath = @".\NormalData.txt";
+            RetCode retCode;
+
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when input list is not empty</summary>
+        public void TestGetNameListFromFile_NonEmptyList()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            string InputPath = @".\NormalData.txt";
+            RetCode retCode;
+
+            NameList.Add(new NameInfo() { LastName = "Archer", GivenName = "Adonis Julius" });
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the file contains normal data</summary>
+        public void TestGetNameListFromFile_NormalData()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            List<NameInfo> ExpectedList = new List<NameInfo>();
+
+            bool Result = false;
+            RetCode retCode;
+
+            string InputPath = @".\NormalData.txt";
+
+            ExpectedList.Add(new NameInfo() { LastName = "Parsons", GivenName = "Janet " });
+            ExpectedList.Add(new NameInfo() { LastName = "Lewis", GivenName = "Vaughn " });
+            ExpectedList.Add(new NameInfo() { LastName = "Archer", GivenName = "Adonis Julius " });
+
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            //compare output list with expected list
+            Result = ListCompare(ref ExpectedList, ref NameList);
+            Assert.IsTrue((retCode == RetCode.SUCCESS) && (NameList.Count > 0) && Result);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the file contains invalid data</summary>
+        public void TestGetNameListFromFile_InvalidData()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            List<NameInfo> ExpectedList = new List<NameInfo>();
+
+            bool Result = false;
+            RetCode retCode;
+
+            string InputPath = @".\InvalidData.txt";
+
+            ExpectedList.Add(new NameInfo() { LastName = "Lewis", GivenName = "Vaughn " });
+            ExpectedList.Add(new NameInfo() { LastName = "Archer", GivenName = "Adonis Julius " });
+            ExpectedList.Add(new NameInfo() { LastName = "Yoder", GivenName = "Shelby Nathan " });
+
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            //compare output list with expected list
+            Result = ListCompare(ref ExpectedList, ref NameList);
+            Assert.IsTrue((retCode == RetCode.SUCCESS) && (NameList.Count > 0) && Result);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the file is empty</summary>
+        public void TestGetNameListFromFile_EmptyFile_1()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            string InputPath = @".\EmptyData.txt";
+            RetCode retCode;
+
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            Assert.IsTrue((retCode == RetCode.NO_VALID_DATA) && (NameList.Count == 0));
+        }
+
+        [TestMethod]
+        /// <summary> Test when the file contains only blank lines</summary>
+        public void TestGetNameListFromFile_EmptyFile_2()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            string InputPath = @".\EmptyDataOnlyBlank.txt";
+            RetCode retCode;
+
+            retCode = InstSortNames.GetNameListFromFile(InputPath, ref NameList);
+
+            Assert.IsTrue((retCode == RetCode.NO_VALID_DATA) && (NameList.Count == 0));
+        }
+
+        [TestMethod]
+        /// <summary> Test when the list is null</summary>
+        public void TestSortNameList_NullList()
+        {
+            List<NameInfo> NameList = null;
+            RetCode retCode;
+
+            retCode = InstSortNames.SortNameList(ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the list contains normal data</summary>
+        public void TestSortNameList_Normal()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            List<NameInfo> ExpectedList = new List<NameInfo>();
+
+            RetCode retCode;
+            bool Result;
+
+            ExpectedList.Add(new NameInfo() { LastName = "Archer", GivenName = "Adonis Julius" });
+            ExpectedList.Add(new NameInfo() { LastName = "Clarke", GivenName = "Hunter Uriah Mathew" });
+            ExpectedList.Add(new NameInfo() { LastName = "Lopez", GivenName = "Mikayla" });
+
+            NameList.Add(new NameInfo() { LastName = "Lopez", GivenName = "Mikayla" });
+            NameList.Add(new NameInfo() { LastName = "Archer", GivenName = "Adonis Julius" });
+            NameList.Add(new NameInfo() { LastName = "Clarke", GivenName = "Hunter Uriah Mathew" });
+            retCode = InstSortNames.SortNameList(ref NameList);
+
+            Result = ListCompare(ref ExpectedList, ref NameList);
+            Assert.IsTrue((retCode == RetCode.SUCCESS) && Result);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the list is null</summary>
+        public void TestDispalyAndWrite_NullList()
+        {
+            List<NameInfo> NameList = null;
+            RetCode retCode;
+
+            retCode = InstSortNames.DispalyAndWrite(ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the list is empty</summary>
+        public void TestDispalyAndWrite_EmptyList()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            RetCode retCode;
+
+            retCode = InstSortNames.DispalyAndWrite(ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the list contains normal data</summary>
+        public void TestDispalyAndWrite_Normal()
+        {
+            List<NameInfo> NameList = new List<NameInfo>();
+            RetCode retCode;
+
+            NameList.Add(new NameInfo() { LastName = "Lopez", GivenName = "Mikayla" });
+            NameList.Add(new NameInfo() { LastName = "Archer", GivenName = "Adonis Julius" });
+            NameList.Add(new NameInfo() { LastName = "Clarke", GivenName = "Hunter Uriah Mathew" });
+            retCode = InstSortNames.DispalyAndWrite(ref NameList);
+
+            Assert.IsTrue(retCode == RetCode.SUCCESS);
+        }
+    }
+
+    [TestClass]
+    public class ValidityCheck_UnitTest
+    {
+        private readonly ValidityCheck instValidityCheck;
+
+        public ValidityCheck_UnitTest() => instValidityCheck = new ValidityCheck();
+
+        [TestMethod]
+        /// <summary> Test when the command line args is null</summary>
+        public void TestValidityCheck_InvalidArgs()
+        {
+            RetCode retCode;
+            string[] astArgs = null;
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.INVALID_PARAM);
+        }
+
+        [TestMethod]
+        /// <summary> Test when no command line args is given</summary>
+        public void TestValidityCheck_NoInputFile()
+        {
+            RetCode retCode;
+            string[] astArgs = { };
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.INPUT_FILE_NAME_EMPTY);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the data file does not exist</summary>
+        public void TestValidityCheck_InexistingInputFile()
+        {
+            RetCode retCode;
+            string[] astArgs = { @"C:\test.txt" };
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.INPUT_FILE_INEXISTING);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the file path does not exist</summary>
+        public void TestValidityCheck_InexistingPath()
+        {
+            RetCode retCode;
+            string[] astArgs = { @"A:\Users\lhy\source\repos\ConsoleApp1\ConsoleApp1\test.txt" };
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.INPUT_FILE_INEXISTING);
+        }
+
+        [TestMethod]
+        /// <summary> Test when the file name is illegal</summary>
+        public void TestValidityCheck_IllegalFileName()
+        {
+            RetCode retCode;
+            string[] astArgs = { "/|\\~!@#$%^<>?" };
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.INPUT_FILE_INEXISTING);
+        }
+
+        [TestMethod]
+        /// <summary> Test with the normal data in absolute path</summary>
+        public void TestValidityCheck_NormalAbsoluteFilePath()
+        {
+            RetCode retCode;
+            string[] astArgs = { @"C:\NormalData.txt" };
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.SUCCESS);
+        }
+
+        [TestMethod]
+        /// <summary> Test with normal data in relative path</summary>
+        public void TestValidityCheck_NormalRelativeFilePath()
+        {
+            RetCode retCode;
+            string[] astArgs = { @".\NormalData.txt" };
+
+            retCode = instValidityCheck.ParameterCheck(astArgs);
+
+            Assert.IsTrue(retCode == RetCode.SUCCESS);
+        }
+    }
+}
---
+++ NameSorter/NameSorter/NameSorter.cs
@@ -0,0 +1,206 @@
+﻿/// <summary>
+/// File name: name-sorter.cs
+/// Description: sort a list of names according to the last name and given name
+/// Author: Haiying Lou
+/// Date: 02-11-2018
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NameSorterApp
+{
+    public enum RetCode
+    {
+        SUCCESS = 0,
+        INVALID_PARAM,
+        INPUT_FILE_NAME_EMPTY,
+        INPUT_FILE_INEXISTING,
+        NO_VALID_DATA
+    };
+
+    public class NameInfo
+    {
+        public String LastName { get; set; }
+        public String GivenName { get; set; }
+    }
+
+    /// <summary>sort a list of names according to the last name and given name</summary>
+    public class SortNames
+    {
+        /// <summary>read names from file and put names into the given list</summary>
+        /// <param name="Path">input file path</param>
+        /// <param name="OutputNameList">unsorted name list</param>
+        /// <returns>INVALID_PARAM, NO_VALID_DATA, SUCCESS</returns>
+        public RetCode GetNameListFromFile(string Path, ref List<NameInfo> OutputNameList)
+        {
+            // The given name list should be an empty list.
+            if ((Path == null) || (OutputNameList == null) || (OutputNameList.Count > 0))
+            {
+                Console.WriteLine("Program Error: INVALID_PARAM");
+                return RetCode.INVALID_PARAM;
+            }
+
+            using (StreamReader Reader = new StreamReader(Path))
+            {
+                // Read the file line by line. 
+                while (!Reader.EndOfStream)
+                {
+                    string Line = Reader.ReadLine();
+
+                    // Remove redundant whitespace.
+                    Line = Line.Trim();
+                    Line = Regex.Replace(Line, @"\s+", " ");
+
+                    // Ingore the blank line.
+                    if (Line == "")
+                    {
+                        continue;
+                    }
+
+                    // Check if it is valid name.
+                    // Valid name format: 1-3 given name, 1 last name,only include letters and whitespace.             
+                    if (!Regex.IsMatch(Line, @"^([a-zA-Z]+\s){1,3}[a-zA-Z]+$"))
+                    {
+                        Console.WriteLine("Ingore this invalid name: {0}", Line);
+                        continue;
+                    }
+
+                    // Extract given name and last name from the line.
+                    string GivenName = Regex.Match(Line, @"^([a-zA-Z]+\s){1,3}").Value;
+                    string LastName = Regex.Match(Line, @"[a-zA-Z]+$").Value;
+
+                    // Put the extracted name into the given list.
+                    OutputNameList.Add(new NameInfo() { LastName = LastName, GivenName = GivenName });
+                }
+
+                if (OutputNameList.Count == 0)
+                {
+                    Console.WriteLine("There are no valid names in the file.");
+                    return RetCode.NO_VALID_DATA;
+                }
+            }
+
+            return RetCode.SUCCESS;
+        }
+
+        /// <summary>sort the name list, save the sorted results in the same list</summary>
+        /// <param name="NameList">input unsorted list and output sorted list</param>
+        /// <returns>INVALID_PARAM, SUCCESS</returns>
+        public RetCode SortNameList(ref List<NameInfo> NameList)
+        {
+            if ((NameList == null) || (NameList.Count == 0))
+            {
+                Console.WriteLine("Program Error: INVALID_PARAM");
+                return RetCode.INVALID_PARAM;
+            }
+
+            // Use the OrderBy method to sort the names in NameList.
+            // Save the sorted names in NameList.
+            NameList = NameList.OrderBy(a => a.LastName).ThenBy(a => a.GivenName).ToList();
+
+            return RetCode.SUCCESS;
+        }
+
+        /// <summary>display sorted names and write them to a file in current work directory</summary>
+        /// <param name="InputNameList">sorted name list</param>
+        /// <returns>INVALID_PARAM, SUCCESS</returns>
+        public RetCode DispalyAndWrite(ref List<NameInfo> InputNameList)
+        {
+            if ((InputNameList == null) || (InputNameList.Count == 0))
+            {
+                Console.WriteLine("Program Error: INVALID_PARAM");
+                return RetCode.INVALID_PARAM;
+            }
+
+            Console.WriteLine("The sorted names are below:");
+
+            using (StreamWriter Writer = new StreamWriter(@"sorted-names-list.txt", false))
+            {
+                // Display the sorted names on screen and write into a TXT file.
+                foreach (NameInfo Name in InputNameList)
+                {
+                    Console.WriteLine(Name.GivenName + Name.LastName);
+                    Writer.WriteLine(Name.GivenName + Name.LastName);
+                }
+            }
+
+            return RetCode.SUCCESS;
+        }
+    }
+
+    /// <summary>parameters validity check</summary>
+    public class ValidityCheck
+    {
+        /// <summary>parameters validity check</summary>
+        /// <param name="args">command line arguements needed to check</param>
+        /// <returns>INVALID_PARAM, INPUT_FILE_NAME_EMPTY, INPUT_FILE_INEXISTING, SUCCESS</returns>
+        public RetCode ParameterCheck(string[] args)
+        {
+            if (args == null)
+            {
+                Console.WriteLine("Program Error: INVALID_PARAM");
+                return RetCode.INVALID_PARAM;
+            }
+
+            // Only support single arg from command line now.
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: name-sorter <FilePath>");
+                return RetCode.INPUT_FILE_NAME_EMPTY;
+            }
+
+            if (!File.Exists(@args[0]))
+            {
+                Console.WriteLine("The file {0} does not exist", args[0]);
+                return RetCode.INPUT_FILE_INEXISTING;
+            }
+
+            return RetCode.SUCCESS;
+        }
+    }
+
+    class NameSorter
+    {
+        /// <summary>name-sorter entry point</summary>
+        /// <param name="args">command line arguments</param>
+        static void Main(string[] args)
+        {
+            RetCode enRet;
+            ValidityCheck cCheck = new ValidityCheck();
+
+            if (RetCode.SUCCESS != cCheck.ParameterCheck(@args))
+            {
+                return;
+            }
+
+            SortNames sn = new SortNames();
+
+            List<NameInfo> NameList = new List<NameInfo>();
+
+            // Get the names from file and put them into NameList.
+            enRet = sn.GetNameListFromFile(@args[0], ref NameList);
+            if (enRet != RetCode.SUCCESS)
+            {
+                return;
+            }
+
+            // Sort the names in NameList accoring to last name, given name.
+            enRet = sn.SortNameList(ref NameList);
+            if (enRet != RetCode.SUCCESS)
+            {
+                return;
+            }
+
+            // Display the sorted name list on screen and write into a TXT file.
+            enRet = sn.DispalyAndWrite(ref NameList);
+            if (enRet != RetCode.SUCCESS)
+            {
+                return;
+            }
+        }
+    }
+}
